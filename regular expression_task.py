2984e718bb9aa1fa2b3badebdd544d48199562cd
Mohd. Farhan Israk Soumik <61477104+farhan1503001@users.noreply.github.com>
2021-05-15 01:41:02 +0600

    Add files via upload

---
+++ regular expression_task.py
@@ -0,0 +1,17 @@
+import re
+import urllib.request,urllib
+import ssl
+#Preparing ssl certificates
+ctx=ssl.create_default_context()
+ctx.check_hostname=False
+ctx.verify_mode=ssl.CERT_NONE
+#Reading the data from web
+url='http://py4e-data.dr-chuck.net/regex_sum_1243931.txt'
+data=urllib.request.urlopen(url,context=ctx).read().decode()
+#print(data)
+numbers=re.findall('[0-9]+',data)
+sum=0
+print(len(numbers))
+for num in numbers:
+    sum=sum+int(num)
+print(sum)
